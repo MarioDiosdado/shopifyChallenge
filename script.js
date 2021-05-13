b9010015e6bbebb15ec96872fc102b69e4735f56
Mario Diosdado <diosdado.mario@gmail.com>
2021-05-13 07:09:36 -0500

    Reorganized code on js file

--- script.js
+++ script.js
@@ -3,108 +3,70 @@ let movieTitles = [];
 let nominatedIDs = [];
 let resultsID = [];
 let movieObjects = JSON.parse(localStorage.getItem("nominated") || "[]");
-let movieTitle;
-let year;
-let imdbID;
-let poster;
 
-//In this section the cards for nominated movies are created
-function renderNominationsCard(movieValue, j) {
-    let movieElement = $(`
-    <div class="card" id="cardNominations" style="width: 14rem;">
-        <img src="${movieObjects[j].poster}" class="card-img-top cardNom mx-auto d-block" alt="Searched Movie">
-        <div class="image_overlay">
-            <img src="assets/dundie.png" alt="dundie">
-            <div class="image_title">${movieObjects[j].title}</div>
-            <p class="card-text image_year">${movieObjects[j].year}</p>
-            <div> <button class="rmvBtn btn btn-danger" id="${movieObjects[j].id}" value="${movieValue}">Remove</button></div>
-        </div>
-    </div>
-    `)
-    return movieElement;
-}
-
-//In this section the cards for searched movies are created, checking first if button should be enabled or disabled
-function renderSearchCard() {
-    let condition;
-    if (movieObjects.filter(e => e.id === imdbID).length <= 0) {
-        condition = "enabled";
-    } else {
-        condition = "disabled"
+//With this function when the page loads it checks local storage and presents whatever is there
+window.onload = () => {
+    renderNominatedList();
+    if (movieObjects.length >= 5) {
+        $('#nominationsReady').addClass('fiveGuysDone').removeClass('fiveGuys');
     }
-    let movieElement2 = $(`
-    <div class="card searchCard" style="width: 10rem;">
-        <img src="${poster}" class="card-img-top cardSearch mx-auto d-block" alt="Nominated movie">
-        <div class="image_overlay">
-        <div class="image_title">${movieTitle}</div>
-        <p class="card-text image_year">${year}</p>
-        <div><button class="nomBtn btn btn-primary btn-sm ${condition}" id="${imdbID}" value="${movieTitle + " " + year}" poster="${poster}" title="${movieTitle}" year="${year}" >Nominate</button></div>
-    </div>
-    </div>
-    `)
-    return movieElement2;
-}
+};
+
+//This on click event searches for moves as we type
+$("#movie-form").on("keyup", event => {
+    event.preventDefault();
+    let movie = $("#movie").val();
+    displayMovieInfo(movie);
+})
 
 //This functions does the Ajax call to get our data from OMDB
-function displayMovieInfo(movie) {
+async function displayMovieInfo(movie) {
     $("#movieList").empty();
     let queryURL = "https://www.omdbapi.com/?s=" + movie + "&apikey=trilogy";
-    $.ajax({
+    const response = await $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response) {
-        resultsID = [];
-        for (i = 0; i <= 4; i++) {
-            movieTitle = response.Search[i].Title;
-            year = response.Search[i].Year;
-            imdbID = response.Search[i].imdbID;
-            poster = response.Search[i].Poster;
-            $("#movieList").append(renderSearchCard());
-            resultsID.push(imdbID);
-        }
-        for (n = 0; n < resultsID.length; n++) {
-            if (nominatedIDs.includes(resultsID[n]) === true) {
-                $("#" + resultsID[n]).prop("disabled", true);
+    })
+    resultsID = [];
+    for (i = 0; i <= 4; i++) {
+        const { Title, Year, imdbID, Poster } = response.Search[i];
+        $("#movieList").append(renderSearchCard(Title, Year, imdbID, Poster));
+        resultsID.push(imdbID);
+    }
+
+    //Here we create an on click event to nominate movies, we also populate arrays that are used for validation and set items in local storage
+    $(".nomBtn").on("click", function () {
+        if ((movieTitles.indexOf(this.value) === -1) && (movieObjects.filter(e => e.id === this.id).length <= 0) && (movieObjects.length <= 4)) {
+            movieTitles.push(this.value);
+            nominatedIDs.push(this.id);
+            let movieObject = {
+                title: this.title,
+                id: this.id,
+                year: this.getAttribute("year"),
+                poster: this.getAttribute("poster"),
+                value: this.title + " " + this.getAttribute("year")
             }
-        }
-        //Here we create an on click event to nominate movies, we also populate arrays that are used for validation and set items in local storage
-        $(".nomBtn").on("click", function () {
-            if ((movieTitles.indexOf(this.value) === -1) && (movieObjects.filter(e => e.id === this.id).length <= 0) && (movieObjects.length <= 4)) {
-                movieTitles.push(this.value);
-                nominatedIDs.push(this.id);
-                let movieObject = {
-                    title: "",
-                    year: "",
-                    poster: "",
-                    id: "",
-                    value: ""
-                }
-                movieObject["title"] = this.title;
-                movieObject["year"] = this.getAttribute("year");
-                movieObject["poster"] = this.getAttribute("poster");
-                movieObject["id"] = this.id;
-                movieObject["value"] = this.title + " " + year;
-                movieObjects.push(movieObject);
-                localStorage.setItem("nominated", JSON.stringify(movieObjects));
-                $(this).prop("disabled", true);
-                //This conditional shows a banner if there are 5 movies already on the nominations list
-                if (movieObjects.length >= 5) {
-                    $('#nominationsReady').addClass('fiveGuysDone').removeClass('fiveGuys');
-                }
-                renderNominatedList();
+            movieObjects.push(movieObject);
+            localStorage.setItem("nominated", JSON.stringify(movieObjects));
+            $(this).prop("disabled", true);
+            //This conditional shows a banner if there are 5 movies already on the nominations list
+            if (movieObjects.length >= 5) {
+                $('#nominationsReady').addClass('fiveGuysDone').removeClass('fiveGuys');
             }
-        })
-    });
+            renderNominatedList();
+        }
+    })
 }
+
 //This function refreshes the nomination list and calls for cards to be created and presented on the screen
-function renderNominatedList() {
+const renderNominatedList = () => {
     if (movieObjects.length <= 5) {
         let list = $("#nominated-list");
         list.empty();
-        for (j = 0; j < movieObjects.length; j++) {
-            let movieValue = movieTitles[j];
-            list.append(renderNominationsCard(movieValue, j));
-        }
+        movieObjects.forEach((item, index) => {
+            let movieValue = movieTitles[index];
+            list.append(renderNominationsCard(movieValue, index));
+        })
     }
     //With this on click event on the remove button movies are removed from the nomination list and from local storage
     $(".rmvBtn").on("click", function () {
@@ -122,16 +84,40 @@ function renderNominatedList() {
         renderNominatedList();
     })
 }
-//This on click event searches for moves as we type
-$("#movie-form").on("keyup", function (event) {
-    event.preventDefault();
-    let movie = $("#movie").val();
-    displayMovieInfo(movie);
-})
-//With this function when the page loads it checks local storage and presents whatever is there
-window.onload = () => {
-    renderNominatedList();
-    if (movieObjects.length >= 5) {
-        $('#nominationsReady').addClass('fiveGuysDone').removeClass('fiveGuys');
+
+//In this section the cards for searched movies are created, checking first if button should be enabled or disabled
+const renderSearchCard = (Title, Year, imdbID, Poster) => {
+    let condition;
+    if (movieObjects.filter(e => e.id === imdbID).length <= 0) {
+        condition = "enabled";
+    } else {
+        condition = "disabled"
     }
-};
+    let movieElement2 = $(`
+    <div class="card searchCard" style="width: 10rem;">
+        <img src="${Poster}" class="card-img-top cardSearch mx-auto d-block" alt="Nominated movie">
+        <div class="image_overlay">
+        <div class="image_title">${Title}</div>
+        <p class="card-text image_year">${Year}</p>
+        <div><button class="nomBtn btn btn-primary btn-sm ${condition}" id="${imdbID}" value="${Title + " " + Year}" poster="${Poster}" title="${Title}" year="${Year}" >Nominate</button></div>
+    </div>
+    </div>
+    `)
+    return movieElement2;
+}
+
+//In this section the cards for nominated movies are created
+const renderNominationsCard = (movieValue, index) => {
+    let movieElement = $(`
+    <div class="card" id="cardNominations" style="width: 14rem;">
+        <img src="${movieObjects[index].poster}" class="card-img-top cardNom mx-auto d-block" alt="Searched Movie">
+        <div class="image_overlay">
+            <img src="assets/dundie.png" alt="dundie">
+            <div class="image_title">${movieObjects[index].title}</div>
+            <p class="card-text image_year">${movieObjects[index].year}</p>
+            <div> <button class="rmvBtn btn btn-danger" id="${movieObjects[index].id}" value="${movieValue}">Remove</button></div>
+        </div>
+    </div>
+    `)
+    return movieElement;
+}
